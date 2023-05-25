a3fc257f99e9102303bff0a539e761684e66c3b6
madVillain6 <levanbichinashvili5@gmai.com>
2023-05-25 17:49:04 +0400

    Feat: Create user Controller

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const userRoute = require("./routes/userRoute");
 
 const app = express();
 
@@ -10,6 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//route middleware
+app.use("/api/users", userRoute);
 
 //Routes
 app.get("/", (req, res) => {
