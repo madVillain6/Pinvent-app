c7bd0ed3fd0c0ce105d5598c02946b26f391bc12
madVillain6 <levanbichinashvili5@gmai.com>
2023-06-01 20:05:02 +0400

    Feat: login status

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -151,9 +151,25 @@ const getUser = asyncHandler(async (req, res) => {
   }
 });
 
+// get login status
+
+const loginStatus = asyncHandler(async (req, res) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json(false);
+  }
+  const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+  if (verified) {
+    return res.json(true);
+  }
+  return res.json(false);
+});
+
 module.exports = {
   registerUser,
   loginUser,
   logout,
   getUser,
+  loginStatus,
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-
 const {
   registerUser,
   loginUser,
   logout,
   getUser,
+  loginStatus,
 } = require("../controllers/userController");
 const protect = require("../middleware/authMiddleware");
 
@@ -13,5 +13,6 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.get("/logout", logout);
 router.get("/getuser", protect, getUser);
+router.get("/loggedin", loginStatus);
 
 module.exports = router;
