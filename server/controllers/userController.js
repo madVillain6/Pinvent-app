27501624a9af6264170469b68220569764cc6c5d
madVillain6 <levanbichinashvili5@gmai.com>
2023-06-01 14:53:21 +0400

    Feat: created userController, errorHandler,

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,47 @@
-const registerUser = async (req, res) => {
-  res.send("Register User");
-};
+const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+
+const registerUser = asyncHandler(async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  // Validation
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("please fill all required fields");
+  }
+  if (password.length < 6) {
+    throw new Error("password must be up to 6 characters");
+  }
+
+  const userExsists = await User.findOne({ email });
+  if (userExsists) {
+    res.status(400);
+    throw new Error("email has already been used");
+  }
+
+  // create new user
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    const { _id, name, email, photo, phone, bio } = user;
+    res.status(201).json({
+      _id,
+      name,
+      email,
+      photo,
+      phone,
+      bio,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data");
+  }
+});
+
 module.exports = {
   registerUser,
 };
--- server/server.js
+++ server/server.js
@@ -4,8 +4,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const userRoute = require("./routes/userRoute");
+const errorHandler = require("./middleware/errorMiddleware");
 
 const app = express();
+
+// doten config
 
 //middleware
 app.use(express.json());
@@ -20,13 +23,17 @@ app.get("/", (req, res) => {
   res.send("home page");
 });
 
+//Error middleware
+app.use(errorHandler);
+
+//Connect to DB and start server
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`server runing on port ${PORT}`);
+      console.log(`server running on port ${PORT}`);
     });
   })
   .catch((err) => console.log(err));
