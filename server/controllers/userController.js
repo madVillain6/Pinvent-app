544e8fbd335abfe7ec5bb1a5e3695c34d2c57f2f
madVillain6 <levanbichinashvili5@gmai.com>
2023-06-01 18:12:33 +0400

    Feat: Logout user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -114,7 +114,23 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 });
 
+//logout
+
+const logout = asyncHandler(async (req, res) => {
+  res.cookie("token", "", {
+    path: "/",
+    httpOnly: true,
+    expires: new Date(0),
+    sameSite: "none",
+    secure: true,
+  });
+  return res.status(200).json({
+    message: "succsessfully logged out",
+  });
+});
+
 module.exports = {
   registerUser,
   loginUser,
+  logout,
 };
